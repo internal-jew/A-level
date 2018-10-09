ec26ed57175a9f5a208d8abfb964e37911f1a89d
vitalygrishin27 <vitalygrishin27@gmail.com>
2018-10-09 17:13:59 +0400

    At the begininng second

--- src/MultiplyKaracuba.java
+++ src/MultiplyKaracuba.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class MultiplyKaracuba {
+    public static void main(String[] args) {
+
+    }
+
+    private static long multiply(long first, long second){
+
+
+
+
+
+        return 0;
+    }
+
 }
